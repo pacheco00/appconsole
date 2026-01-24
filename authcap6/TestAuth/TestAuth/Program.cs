c8d0513b71544a0c8e2e37f6dbf6ccd3517298d7
edgardo.nvf <edgardo.nvf@gmail.com>
2026-01-23 19:51:54 -0600

    viernes

---
+++ authcap6/TestAuth/TestAuth/Program.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Identity.Client;
+
+/*Declarando cliente_id y el tenant_id*/
+string _clientId = "<client_id>";
+string _tenantId = "<tenant_id>";
+
+/*Definimos los alcances  y creamos el cliente de auth*/
+string[] _scopes = { "User.Read" };
+
+/*Cliente de MSAL de microsoft*/
+var app = PublicClientApplicationBuilder
+          .Create(_clientId)
+          .WithAuthority(AzureCloudInstance.AzurePublic, _tenantId)
+          .WithDefaultRedirectUri()
+          .Build();
+
+/*Código que solicita el token de acceso*/
+
+AuthenticationResult result;
+
+try
+{
+    /*Intentar obtener el token de acceso desde cache*/
+    var accounts= await app.GetAccountsAsync();
+    result = await app.AcquireTokenSilent(_scopes, accounts.FirstOrDefault())
+        .ExecuteAsync();
+
+}
+catch (MsalUiRequiredException ex) 
+{ 
+    Console.WriteLine($"Cache token no esta {ex.Message}");
+    result = await app.AcquireTokenInteractive(_scopes).ExecuteAsync();
+
+}
+
+
+/*Imprimir en consola el token de acceso*/
+Console.WriteLine($"Access Token: \n {result.AccessToken}");
+
+
+
+
+
+
+
+
